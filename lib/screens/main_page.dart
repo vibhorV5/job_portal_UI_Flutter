5c180defbb5d5f5052bf5fee16d330c3b31f36e8
Vibhor Vats <vibhordev7@gmail.com>
2022-02-28 12:05:15 +0530

    5000

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -143,7 +143,7 @@ class _MainPageState extends State<MainPage> {
           ),
 
           const SizedBox(
-            height: 50,
+            height: 50000,
           ),
 
           Container(
